bf00832745e9c5d9ad021fa4fa93ada2ffe925c1
mubashirshahzad07 <mubashirs1234567@gmail.com>
2026-05-23 15:40:16 +0500

    Initial commit: Add the project files

---
+++ src/main/java/patient/management/system/Main.java
@@ -0,0 +1,12 @@
+package patient.management.system;
+
+public class Main extends Parent {
+
+    public static void main(String[] args) {
+        Main v = new Main();
+    }
+}
+
+class Parent {
+    int superValue = 2;
+}
---
+++ src/main/java/patient/management/system/service/EmailService.java
@@ -0,0 +1,68 @@
+package patient.management.system.service;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+import java.util.Properties;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EmailService {
+
+    public static void main(String[] args) {
+        sendEmail("smtptrial63@gmail.com, another@gmail.com");
+    }
+
+    /**
+     * sends email given the recipient addresses
+     * @param to recipient addresses (format: "email(1), email(2), email(3), ... , email(n)")
+     */
+    public static void sendEmail(String to) {
+        if (!isValidEmail(to)) {
+            throw new RuntimeException("Email address is not valid.");
+        }
+
+        String host = "smtp.gmail.com";
+        String from = "patientmanagementsystem2026@gmail.com";
+        String password = "fuwd uhtl luxe aude";
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.BCC, InternetAddress.parse(to));
+            message.setSubject("Hello");
+            message.setText("This is test email.");
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * checks if the emails are valid
+     * @param to recipient addresses (format: "email(1), email(2), email(3), ... , email(n)")
+     * @return true if emails are valid, false otherwise
+     */
+    public static boolean isValidEmail(String to) {
+        String[] recipientEmails = to.split(",");
+
+        for (String recipientEmail : recipientEmails) {
+            if (!recipientEmail.contains("@gmail.com")) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
