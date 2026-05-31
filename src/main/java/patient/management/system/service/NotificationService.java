2d3a6274bfd2ddf1d114a3d136bb6251b3163361
mubashirshahzad07 <mubashirs1234567@gmail.com>
2026-05-31 11:00:22 +0500

    Add: NotificationService

--- src/main/java/patient/management/system/service/NotificationService.java
+++ src/main/java/patient/management/system/service/NotificationService.java
@@ -1,11 +1,13 @@
 package patient.management.system.service;
 
+import patient.management.system.dao.AppointmentDAO;
 import patient.management.system.dao.NotificationDAO;
 
 public class NotificationService {
-    public void sendEmail(String to) {
+
+    public void sendEmail() {
         try {
-            NotificationDAO.sendEmail(to);
+            NotificationDAO.sendEmail(new AppointmentDAO().emailsToNotify());
         } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
