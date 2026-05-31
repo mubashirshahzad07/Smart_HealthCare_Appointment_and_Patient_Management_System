c7d20307ffd077f0e961a3446208e5975d19e358
mubashirshahzad07 <mubashirs1234567@gmail.com>
2026-05-31 11:00:40 +0500

    Add: NotificationService

--- src/main/java/patient/management/system/dao/AppointmentDAO.java
+++ src/main/java/patient/management/system/dao/AppointmentDAO.java
@@ -17,7 +17,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class AppointmentDAO {
     private final ObjectMapper mapper = new ObjectMapper();
