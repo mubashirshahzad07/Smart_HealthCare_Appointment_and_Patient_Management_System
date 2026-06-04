a473baeb85fc2660e152b5e102dfbe433cc6832b
mubashirshahzad07 <mubashirs1234567@gmail.com>
2026-06-04 19:18:53 +0500

    Update

--- src/main/java/patient/management/system/service/NotificationService.java
+++ src/main/java/patient/management/system/service/NotificationService.java
@@ -7,8 +7,16 @@ public class NotificationService {
 
     public void sendReminder() {
         try {
+
             String emails = new AppointmentDAO().emailsToNotify();
-            NotificationDAO.sendEmail(emails, NotificationDAO.REMINDER_MESSAGE);
+            Thread reminderThread = new Thread() {
+                @Override
+                public void run() {
+                    NotificationDAO.sendEmail(emails, NotificationDAO.REMINDER_MESSAGE);
+                }
+            };
+            reminderThread.start();
+
         } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
